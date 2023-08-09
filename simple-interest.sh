b40d261dea98da099c2422304ed4b3d0f4485f17
cheburator3000 <141877290+cheburator3000@users.noreply.github.com>
2023-08-10 00:26:16 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # cheburator3000
    # Input:
    # p, principal amount
    # t, time period in years
